03307f6647efa2fba21400f39e0f0cfe18807b2e
findigital <finley.marcus@gmail.com>
2024-09-10 12:10:22 -0400

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,17 +1,32 @@
 from crewai import Agent
 from textwrap import dedent
-from langchain_openai import ChatOpenAI
-from crewai_tools import PDFSearchTool
 from langchain_groq import ChatGroq
 import os
+from langchain.tools import Tool
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
 
 class CustomAgents:
     def __init__(self, pdf_paths):
-        self.OpenAIGPT35 = ChatOpenAI(model_name="gpt-4o-mini")
-        self.groq_llm=ChatGroq(temperature=0,
+        self.groq_llm = ChatGroq(temperature=0,
              model_name="llama3-70b-8192", api_key=os.getenv("GROQ_API_KEY"))
-        self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4")
-        self.pdf_tools = [PDFSearchTool(pdf=pdf_path) for pdf_path in pdf_paths]
+        self.pdf_tools = [self.create_pdf_search_tool(pdf_path) for pdf_path in pdf_paths]
+
+    def create_pdf_search_tool(self, pdf_path):
+        loader = PyPDFLoader(pdf_path)
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+        embeddings = HuggingFaceEmbeddings()
+        db = FAISS.from_documents(texts, embeddings)
+
+        return Tool(
+            name=f"Search {pdf_path}",
+            func=lambda q: db.similarity_search(q, k=1)[0].page_content,
+            description=f"Useful for searching content in {pdf_path}"
+        )
 
     def document_ingestion_agent(self):
         return Agent(
@@ -92,10 +107,10 @@ class CustomAgents:
         return Agent(
             role="Expert Team and Governance Writing Agent",
             backstory=dedent("""
-                You are the world's best Team and Governance Writing Agent, adept at highlighting the strengths and qualifications of the nonprofit’s team and governance.
+                You are the world's best Team and Governance Writing Agent, adept at highlighting the strengths and qualifications of the nonprofit's team and governance.
             """),
             goal=dedent("""
-                Highlight the nonprofit’s team and governance strengths effectively to demonstrate organizational capacity and credibility.
+                Highlight the nonprofit's team and governance strengths effectively to demonstrate organizational capacity and credibility.
             """),
             tools=self.pdf_tools,
             allow_delegation=False,
